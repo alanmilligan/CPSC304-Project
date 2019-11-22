bb52c694725d987e1aa80f48db58b9c8695fea62
jacqueschen1 <jacqueschen1@gmail.com>
2019-11-22 03:55:26 -0800

    update sql init file

--- src/database/init.sql
+++ src/database/init.sql
@@ -5,14 +5,14 @@ DROP TABLE Reservation;
 DROP TABLE VehicleType;
 DROP TABLE Customer;
 
+CREATE TABLE VehicleType
+ (vtname char(20), features char(140), wrate int, drate int, hrate int, wirate int, dirate int, hirate int, krate int,
+ PRIMARY KEY (vtname));
 
-CREATE TABLE VehicleType 
-(vtname char(20), features char(140), wrate real, drate real, hrate real, wirate real, hirate real, krate real,
-PRIMARY KEY (vtname));
+insert into VehicleType values ('SUV', '5 seats but larger', 500, 100, 15, 50, 10, 2,  5);
+insert into VehicleType values ('Truck', 'Large boy for heavy lifting', 700, 200, 20, 75, 30, 4, 10);
+insert into VehicleType values ('Sedan', '5 seats and stuff',  400, 80, 10, 25, 10, 2, 4);
 
-insert into VehicleType values ('SUV', '5 seats but larger', 500, 100, 15, 50, 2, 5);
-insert into VehicleType values ('Truck', 'Large boy for heavy lifting', 700, 200, 20, 75, 4, 10);
-insert into VehicleType values ('Sedan', '5 seats and stuff', 400, 80, 10, 25, 2, 4);
 
 
 CREATE TABLE Customer
@@ -26,7 +26,7 @@ INSERT INTO Customer values ('6049231234', 'Tragic Bronson', '1031 Apple Dr.', '
 
 
 CREATE TABLE Vehicle
-(vlicense char(7), make char(20), model char(20), year int, color char(20), odometer real, status char(20), vtname char(20), location char(20), city char(20),
+(vlicense char(7), make char(20), model char(20), year int, color char(20), odometer int, status char(20), vtname char(20), location char(20), city char(20),
 PRIMARY KEY (vlicense),
 FOREIGN KEY (vtname) REFERENCES VehicleType);
 
@@ -38,7 +38,7 @@ insert into Vehicle values('2133333', 'Audi', 'R8', 2013, 'Silver', '100000', 'A
 insert into Vehicle values('2123239', 'BMW', 'M3', 2010, 'Blue', '300000', 'Rented', 'Sedan', 'UBC', 'Vancouver');
 insert into Vehicle values('2121139', 'BMW', 'i7', 2014, 'Blue', '300000', 'Rented', 'SUV', 'UBC', 'Vancouver');
 insert into Vehicle values('3433139', 'Dodge', 'RAM', 2014, 'Red', '300000', 'Rented', 'Truck', 'UBC', 'Vancouver');
-insert into Vehicle values('3444139', 'Dodge', 'RAM', 2015, 'Red', '300000', 'In shop', 'Truck', 'UBC', 'Vancouver'); 
+insert into Vehicle values('3444139', 'Dodge', 'RAM', 2015, 'Red', '300000', 'In shop', 'Truck', 'UBC', 'Vancouver');
 insert into Vehicle values('4444433', 'Audi', 'R8', 2013, 'Silver', '300000', 'In shop', 'Sedan', 'Kits', 'Vancouver');
 
 
@@ -46,9 +46,9 @@ insert into Vehicle values('4444433', 'Audi', 'R8', 2013, 'Silver', '300000', 'I
 
 
 
-CREATE TABLE Reservation 
+CREATE TABLE Reservation
 (confNo int, vtname char(20), dlicense char(7), fromDate timestamp, toDate timestamp,
-PRIMARY KEY (confNo), 
+PRIMARY KEY (confNo),
 FOREIGN KEY (dlicense) REFERENCES Customer,
 FOREIGN KEY (vtname) REFERENCES VehicleType);
 
@@ -61,7 +61,7 @@ INSERT INTO Reservation values (000003, 'Sedan', 'B54MK19', TO_TIMESTAMP('2019-0
 
 
 CREATE TABLE Rent
-(rid int, vlicense char(7), dlicense char(7),  fromDate timestamp, toDate timestamp, odometer real, cardName char(30), cardNo int, ExpDate int, confNo int,
+(rid int, vlicense char(7), dlicense char(7),  fromDate timestamp, toDate timestamp, odometer int, cardName char(30), cardNo int, ExpDate int, confNo int,
 PRIMARY KEY (rid),
 FOREIGN KEY (dlicense) REFERENCES Customer,
 FOREIGN KEY (vlicense) REFERENCES Vehicle,
@@ -74,9 +74,8 @@ INSERT INTO Rent values (3, '2948323',  '71A8F39',  TO_TIMESTAMP('2019-07-01 05:
 INSERT INTO Rent values (2, '2133333',  'B54MK19',  TO_TIMESTAMP('2019-07-01 05:10:00', 'YYYY-MM-DD HH24:MI:SS'), TO_TIMESTAMP('2019-07-23 05:10:00', 'YYYY-MM-DD HH24:MI:SS'), 200000, 'Tragic Bronson', 24242424, 082024, NULL);
 
 CREATE TABLE Return
-(rid int, rdate timestamp, odometer real, fulltank char(1), value real, 
+(rid int, rdate timestamp, odometer int, fulltank char(1), value int,
 PRIMARY KEY (rid),
 FOREIGN KEY (rid) REFERENCES Rent);
 
 INSERT INTO Return values (1, TO_TIMESTAMP('2019-03-01 05:10:00', 'YYYY-MM-DD HH24:MI:SS'), 240000, 'Y', 3849723);
-
